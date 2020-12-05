9d95d6bf978e616816feae823eedca13ad008e92
Amrinder Singh <amrinderpal.singh@outlook.com>
2020-12-05 23:11:03 +0530

    Initial commit

---
+++ ContactManager.Data/ContactManagerAppContext.cs
@@ -0,0 +1,22 @@
+﻿using ContactManager.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContactManager.Data
+{
+    public class ContactManagerAppContext : DbContext
+    {
+        public ContactManagerAppContext(DbContextOptions<ContactManagerAppContext> options): base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
+        public DbSet<Contact> Contacts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Contact>().HasKey(x => x.Id);
+        }
+    }
+}
